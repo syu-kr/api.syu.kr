fe7b3fae407d851987f9fcdb9849a527f32ffbd3
0verfl0w767 <tkddbs7631@naver.com>
2023-08-18 02:15:06 +0900

    added detailed conditions

--- src/undergraduate/controller.ts
+++ src/undergraduate/controller.ts
@@ -22,12 +22,7 @@ export class V1Controller {
   public constructor(private readonly Service: V1Service) {}
 
   @Get(':year/:semester/:id')
-  public getProduct(
-    @Param('year') year: string,
-    @Param('semester') semester: string,
-    @Param('id') id: string,
-    @Res() res: Response,
-  ): void {
+  public getProduct(@Param('year') year: string, @Param('semester') semester: string, @Param('id') id: string, @Res() res: Response): void {
     const convertSemester = {
       '1': '1학기 정규',
       '2': '2학기 정규',
@@ -41,33 +36,53 @@ export class V1Controller {
       return
     }
 
-    const dirPath = process.cwd() + '/undergraduate/' + year + '/' + convertSemester[semester]
+    const mainPath = process.cwd() + '/undergraduate/' + year + '/' + convertSemester[semester]
 
-    if (!fs.existsSync(dirPath)) {
+    if (!fs.existsSync(mainPath)) {
       res.status(HttpStatus.NOT_FOUND).json({
         statusCode: HttpStatus.NOT_FOUND,
-        message: year + ' - ' + convertSemester[semester] + ' not found.',
+        message: 'undergraduates file is not found.',
       })
       return
     }
 
-    const undergraduates = JSON.parse(fs.readFileSync(dirPath + '/학부(과).json', 'utf8'))
-    let undergraduate = undergraduates['api'].find(
-      (undergraduate: { [element: string]: string }) => undergraduate['식별번호'] == id,
-    )
+    const subPath = mainPath + '/학부(과).json'
+
+    if (!fs.existsSync(subPath)) {
+      res.status(HttpStatus.NOT_FOUND).json({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'undergraduates list file is not found.',
+      })
+      return
+    }
+
+    const undergraduates = JSON.parse(fs.readFileSync(subPath, 'utf8'))
+
+    const undergraduate = undergraduates['api'].find((undergraduate: { [element: string]: string }) => undergraduate['식별번호'] == id)
 
     if (undergraduate === undefined) {
       res.status(HttpStatus.NOT_FOUND).json({
         statusCode: HttpStatus.NOT_FOUND,
-        message: year + ' - ' + convertSemester[semester] + ' - ' + id + ' not found.',
+        message: 'undergraduate id is not found.',
       })
       return
     }
 
-    const data = JSON.parse(
-      fs.readFileSync(dirPath + '/전체대학/' + undergraduate['학부(과)'] + '.json', 'utf8'),
-    )
+    const dataPath = mainPath + '/전체대학/' + undergraduate['학부(과)'] + '.json'
 
-    res.status(HttpStatus.OK).json(data)
+    if (!fs.existsSync(dataPath)) {
+      res.status(HttpStatus.NOT_FOUND).json({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'undergraduate file is not found.',
+      })
+      return
+    }
+
+    const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'))
+
+    res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      api: data['api'],
+    })
   }
 }
